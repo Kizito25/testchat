5abf9be57fd255a3b40e47a7e9a896a2211fe1e6
devKizito <devkizito@gmail.com>
2022-09-15 12:31:54 +0100

    Created Login Feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { useState, useEffect } from "react";
+import { ChatPanel } from "./components";
+import { Home } from "./pages";
+
+function cl(val) {
+  return console.log(val);
+}
 const App = () => {
+  let [isLoggedIn, setIsLoggedIn] = useState(false);
+  let [username, setUsername] = useState("");
+  let checkIsLoggedIn = () => {
+    if (sessionStorage.getItem("username") !== null) {
+      setUsername(JSON.parse(sessionStorage.getItem("username")));
+      setIsLoggedIn(true);
+    } else {
+      setUsername(JSON.parse(sessionStorage.removeItem("username")));
+      setIsLoggedIn(false);
+    }
+  };
+  useEffect(() => {
+    checkIsLoggedIn();
+  }, []);
   return (
-    <main className="min-h-screen bg-gradient-to-b from-slate-50 to-indigo-50 p-5"></main>
+    <main className="min-h-screen bg-gradient-to-b from-slate-50 to-indigo-50">
+      {isLoggedIn === false ? <Home /> : <ChatPanel />}
+    </main>
   );
 };
 
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as ChatPanel } from "./ChatPanel";
+export { default as ChatSidebar } from "./ChatSidebar";
+export { default as Chat } from "./Chat";
